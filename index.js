b28097564c347bcec452d13e2fba607db59b3763
jannatun67 <jannatunfima67@gmail.com>
2024-12-26 16:31:53 +0600

    My Profile

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const managementDatabase = client.db("managementDB").collection("management");
 
